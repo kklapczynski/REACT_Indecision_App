93963274ef8320cd50a90dfea2f021b11ae514a9
KK <k.klapczynski@gmail.com>
2018-04-12 14:25:46 +0200

    end of lecture 12

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,74 +1,98 @@
-"use strict";
+'use strict';
 
 // this app.js will have JSX used - will be transpiled using babel to public/app.js , that is actually loaded by browser
 
-console.log("App is running!");
+console.log('App is running!');
 
 // JSX - JS XML ( JavaScript syntax extension) - React uses it to build templates
+// lecture 11
 // create app object with title and subtitle
 // use it in template
 // render
+// lecture 12
+// only render subtitle paragraph if it exists
+// add <p> if property 'options' exists: 'Here are your options' or 'No options set'
 var app = {
-    title: "Indecision App",
-    subtitle: "Application to make you decide quicker!"
+    title: 'Indecision App',
+    subtitle: 'Application to make you decide quicker!',
+    options: ['one', 'two']
 };
 var template = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
         app.title
     ),
-    React.createElement(
-        "p",
+    app.subtitle && typeof app.subtitle === 'string' && app.subtitle.length > 0 && React.createElement(
+        'p',
         null,
         app.subtitle
     ),
+    app.options && app.options.length > 0 ? React.createElement(
+        'p',
+        null,
+        'Here are your options:'
+    ) : React.createElement(
+        'p',
+        null,
+        'No options set.'
+    ),
     React.createElement(
-        "ol",
+        'ol',
         null,
         React.createElement(
-            "li",
+            'li',
             null,
-            "Item 1"
+            'Item 1'
         ),
         React.createElement(
-            "li",
+            'li',
             null,
-            "Item 2"
+            'Item 2'
         )
     )
 );
 
 var user = {
-    name: "krzysztof klapczy\u0144ski",
+    name: 'krzysztof klapczy\u0144ski',
     age: 37,
-    location: "Pozna\u0144"
+    location: 'Lisboa'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+    // undefined is returned from function if there is no else statement
+    //  else {
+    //     return `Unknown`;
+    // }
 };
 
 var template2 = React.createElement(
-    "div",
+    'div',
     null,
     React.createElement(
-        "h1",
+        'h1',
         null,
-        user.name.toUpperCase()
+        user.name ? user.name.toUpperCase() : 'unknown'
     ),
-    React.createElement(
-        "p",
+    user.age && user.age >= 18 && React.createElement(
+        'p',
         null,
-        "Age: ",
+        'Age: ',
         user.age
     ),
-    React.createElement(
-        "p",
-        null,
-        "Location: ",
-        user.location
-    )
+    getLocation(user.location)
 );
 
-var htmlElToRenderTemplateIn = document.getElementById("app");
+var htmlElToRenderTemplateIn = document.getElementById('app');
 
 ReactDOM.render(template, htmlElToRenderTemplateIn);
--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@
 console.log(`App is running!`);
 
 // JSX - JS XML ( JavaScript syntax extension) - React uses it to build templates
+// lecture 11
 // create app object with title and subtitle
 // use it in template
 // render
+// lecture 12
+// only render subtitle paragraph if it exists
+// add <p> if property 'options' exists: 'Here are your options' or 'No options set'
 var app = {
     title: `Indecision App`,
-    subtitle: `Application to make you decide quicker!`
+    subtitle: `Application to make you decide quicker!`,
+    options: [`one`, `two`]
 };
 var template = (
     <div>
         <h1>{app.title}</h1> 
-        <p>{app.subtitle}</p>
+        {(app.subtitle && typeof(app.subtitle) === 'string' && app.subtitle.length > 0) && <p>{app.subtitle}</p>}
+        {(app.options && app.options.length > 0) ? <p>Here are your options:</p> : <p>No options set.</p>}
         <ol>
             <li>Item 1</li>
             <li>Item 2</li>
@@ -24,14 +30,24 @@ var template = (
 var user = {
     name: `krzysztof klapczyński`,
     age: 37,
-    location: `Poznań`
+    location: `Lisboa`
+};
+
+function getLocation(location) {
+    if(location) {
+        return <p>Location: {location}</p>;
+    }
+    // undefined is returned from function if there is no else statement
+    //  else {
+    //     return `Unknown`;
+    // }
 };
 
 var template2 = (
     <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ?  user.name.toUpperCase() : `unknown`}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
